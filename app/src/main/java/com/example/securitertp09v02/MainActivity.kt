ca616541eada690eb98e8498709ffe871e9e07fe
Ilyass <110940760+androidcrafting@users.noreply.github.com>
2026-02-13 11:51:39 +0100

    Update MainActivity.kt
    Added a new function to display a welcome message.

--- app/src/main/java/com/example/securitertp09v02/MainActivity.kt
+++ app/src/main/java/com/example/securitertp09v02/MainActivity.kt
@@ -11,10 +11,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        fun afficherAccueil() {
+            println("Bienvenue dans HealthTracker")
         }
+
     }
 }
